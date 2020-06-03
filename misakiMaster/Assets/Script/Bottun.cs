ff506290c5e6f240c1ec167c092755d7bf67c3bf
佐々木美咲 <the.glass007@icloud.com>
2020-06-03 19:47:06 +0900

    Revert "ついに泡の中にバイキンが入りました（あとそっとやばかったパネルを直した）"
    This reverts commit 213a5140db48ef92477b5642666eb963b4f28c4a.

--- misakiMaster/Assets/Script/Bottun.cs
+++ misakiMaster/Assets/Script/Bottun.cs
@@ -9,7 +9,6 @@ public class Bottun : MonoBehaviour
     manager script;
     GameObject FieldObject;
     field FieldScript;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +22,7 @@ public class Bottun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
     }
     public void OnClick()
     {
@@ -46,7 +46,6 @@ public class Bottun : MonoBehaviour
             script.SetTop(script.nowTop, false);
             StartCoroutine(FieldScript.RollYR());
         }
-
 
     }
     public void SetisClicked(bool check)
